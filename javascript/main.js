0ed7f8fdd6099350c1c2ab94697c01cac6c409ef
Мария Шишова <maryshish17@gmail.com>
2025-06-11 11:03:04 +0300

    д

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,16 @@
-window.addEventListener("DOMContentLoaded", () => {
+// фик меню
+window.addEventListener("scroll", () => {
+  const menu = document.querySelector(".main-menu");
+  if (window.scrollY > 250) {
+    menu.classList.add("fixed");
+  } else {
+    menu.classList.remove("fixed");
+  }
+});
+
+//появление контента из блюра
+window.addEventListener("load", () => {
   document.querySelectorAll(".fade-blur").forEach((el) => {
-    // Добавляем класс visible, чтобы запустить анимацию появления
     el.classList.add("visible");
   });
 });
